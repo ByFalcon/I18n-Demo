982fcd947550429e1c0c82268aaf1d5a752cb287
ByFalcon <danielarellanoalabarce@gmail.com>
2019-06-08 10:53:02 +0200

    Initial Commit

---
+++ I18n Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I18n Demo
+//
+//  Created by dam on 08/06/2019.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
