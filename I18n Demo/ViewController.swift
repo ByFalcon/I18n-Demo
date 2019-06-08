8e51ec9396f3a7f5e091c0ddb30b605cef8f2804
ByFalcon <danielarellanoalabarce@gmail.com>
2019-06-08 11:42:20 +0200

    inicial commit

--- I18n Demo/ViewController.swift
+++ I18n Demo/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        populateValues()
+    }
+    
+    var currencyFormater: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
     }
 
+    private func populateValues(){
+        textLabel.text = NSLocalizedString("HELLO_WORLD", comment: "Hello world")
+        currencyLabel.text = currencyFormater.string(from: 10000)
+        dateLabel.text = dateFormatter.string(from: NSDate() as Date)
+        imageView.image = UIImage(named: "flag")
+    }
 
 }
 
